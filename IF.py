928b7735d253543e78e3294590b6e2500b1a2f95
unknown <229166@TECH.WYB.AC.LK>
2025-03-26 20:13:33 +0530

    first commit

---
+++ IF.py
@@ -0,0 +1,12 @@
+num=11
+if(num>5):
+    print("num is grater than 5")
+elif num>10:
+    print("num is grater than 10")
+else:
+    print ("num is not grater than 5")
+    
+num=5
+while (num<20):
+    num+=1
+    print(num)
