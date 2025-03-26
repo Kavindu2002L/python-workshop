70f6d0d37535d13f95a6af8e803dd8604c7fce2b
unknown <229166@TECH.WYB.AC.LK>
2025-03-26 21:14:27 +0530

    first commit

---
+++ List.py
@@ -0,0 +1,14 @@
+num=[1,2,2,3,4,5,6,58,69,7,98,36]
+#num.index(5)
+print(max(num))
+print(min(num))
+print(len(num))
+
+def filter_value(cost):
+    if(cost>350):
+        return True
+    else:
+        return False
+price=[230,480,450,350,370]
+filter_value=filter(filter_value,price)
+print(list (filter_value))
